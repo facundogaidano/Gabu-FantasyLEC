55f6fb79db27d5c60261f374f20d6af3850b1ef1
Facundo Gaidano <gaidano6@gmail.com>
2025-01-19 18:23:31 -0300

    fixed?

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   base: '/Gabu-FantasyLEC/',
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/assets/img',
-          dest: 'assets'
-        }
-      ]
-    })
-  ]
+  plugins: [react()],
 });
