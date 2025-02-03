ebb6e8524e036db1a8f8a34653b6e2f84f72c1f5
Facundo Gaidano <gaidano6@gmail.com>
2025-02-03 18:18:48 -0300

    W3 ended

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 const Leaderboard = () => {
   const players = [
-    { name: 'MrFakon', points: [21,4+3+1] },
-    { name: 'TakRiuto', points: [15,3+2+2] },
-    { name: 'Isehnia', points: [15,1+5+2] },
-    { name: 'Spike', points: [16,4+3+1] },
-    { name: 'Damian', points: [22,4+5+1] },
-    { name: 'Raledro', points: [18,4+3+1] },
-    { name: 'Ursus', points: [20,3+2+1] },
-    { name: 'Papelon', points: [16,4+2] },
-    { name: 'Devmon', points: [13,4+3+1] },
-    { name: 'Eduamen', points: [15,1+2+1] },
-    { name: 'Joe', points: [16,5+1] },
-    { name: 'Carlos', points: [19,4+5+1] },
-    { name: 'Grimo', points: [0,4+3+2] }
+    { name: 'MrFakon', points: [21,4+3+1+2] },
+    { name: 'TakRiuto', points: [15,3+2+2+2] },
+    { name: 'Isehnia', points: [15,1+5+2+2] },
+    { name: 'Spike', points: [16,4+3+1+2] },
+    { name: 'Damian', points: [22,4+5+1+2] },
+    { name: 'Raledro', points: [18,4+3+1+2] },
+    { name: 'Ursus', points: [20,3+2+1+2] },
+    { name: 'Papelon', points: [16,4+2+2] },
+    { name: 'Devmon', points: [13,4+3+1+2] },
+    { name: 'Eduamen', points: [15,1+2+1+2] },
+    { name: 'Joe', points: [16,5+1+1] },
+    { name: 'Carlos', points: [19,4+5+1+2] },
+    { name: 'Grimo', points: [0,4+3+2+2] }
   ];
 
   const playersWithTotalPoints = players.map(player => ({
